52fc483f4e127ad4b0a74ad3f9670f1bf27be791
renemel <rene.melendeza@gmail.com>
2020-05-12 16:40:30 -0600

    Pantalla Producto Agregada

---
+++ CrowingRooster/app/src/main/java/com/cccm/crowingrooster/CatalogueFragment.kt
@@ -0,0 +1,40 @@
+package com.cccm.crowingrooster
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_catalogue.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+private lateinit var linearLayoutManager: LinearLayoutManager
+
+class CatalogueFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_catalogue, container, false)
+
+
+    }
+
+
+
+}
+
+
