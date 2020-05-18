5072f601ac906e98dbf66edddc5f7d60287b4e10
Slackoth <00082318@uca.edu.sv>
2020-05-17 22:46:03 -0600

    [Generic RecyclerView COMPLETED]

--- CrowingRooster/app/src/main/java/com/cccm/crowingrooster/SuccessfulSalesFragment.kt
+++ CrowingRooster/app/src/main/java/com/cccm/crowingrooster/SuccessfulSalesFragment.kt
@@ -18,14 +18,8 @@ import com.cccm.crowingrooster.databinding.FragmentSuccessfulSalesBinding
  */
 class SuccessfulSalesFragment : Fragment() {
 
-    companion object {
-        const val TAG: String = "SuccessfulSalesFragment"
-    }
-
-    var clientName: MutableList<String> = mutableListOf()
-    var modelName: MutableList<String> = mutableListOf()
-    var quantityNum: MutableList<Int> = mutableListOf()
-    var imgUrl: MutableList<String> = mutableListOf()
+    lateinit var recyclerView: RecyclerView
+    var saleList: MutableList<Any> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,55 +27,70 @@ class SuccessfulSalesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_successful_sales, container, false)
-        val bind = DataBindingUtil.inflate<FragmentSuccessfulSalesBinding>(inflater,R.layout.fragment_successful_sales,
-        container, false)
+        val bind = DataBindingUtil.inflate<FragmentSuccessfulSalesBinding>(
+            inflater, R.layout.fragment_successful_sales,
+            container, false
+        )
 
-        Log.d(TAG, "onCreate: Started")
+        //Log.d(TAG, "onCreate: Started")
         (activity as MainActivity).supportActionBar?.title = getString(R.string.successful_sales)
-        return bind.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initImageBitmap()
-    }
+        recyclerView = bind.recyclerView
+        saleList.addAll(
+            listOf(
+                Sale(
+                    "Rene", "A", 5,
+                    "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB"
+                ),
+                Sale(
+                    "Luis",
+                    "B",
+                    10,
+                    "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB"
+                ),
+                Sale(
+                    "Christian",
+                    "C",
+                    15,
+                    "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB"
+                ),
+                Sale(
+                    "Pipo",
+                    "D",
+                    20,
+                    "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB"
+                )
+            )
+        )
+        //Creating the RecyclerView Adapter
+        val adapter = object : GenericRecyclerViewAdapter<Any>(saleList, requireContext()) {
+            override fun getViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder {
+                return ViewHolderFactory.bindView(view, viewType)
+            }
 
-    private fun initImageBitmap() {
-        Log.d(TAG, "onCreate: preparing bitmaps")
-        val client = listOf<String>("JOSE LUIS ALBARRAN REQUENA","MIGUEL MATESANZ ARTIGAS",
-        "FELIX BARRIO RECIO","LUCIA LLORCA CAÑO","ANTONIA AGUERA PAN","CONCEPCION IBAÑEZ POSADA")
-        val model = listOf<String>("22F-30","42-330","42R-330","24-350","24R-350","27-540")
-        val quantity = listOf<Int>(1,20,300,4000,50000,6000000)
-        var url = listOf("https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB",
-        "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB",
-        "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB",
-        "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB",
-        "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB",
-        "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/54371128_2605774029452750_1474735591550615552_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=0YEa9J_uk_EAX_DjfCX&_nc_ht=scontent-mia3-1.xx&oh=b4f0a9a730d6915d632424f62451adf1&oe=5EE56BEB")
+            override fun getLayoutId(position: Int, obj: Any): Int {
+                return when (obj) {
+                    is Sale -> R.layout.sale_item_layout
+                    else -> R.layout.sale_item_layout
+                }
+            }
+        }
 
-        clientName.addAll(client)
-        modelName.addAll(model)
-        quantityNum.addAll(quantity)
-        imgUrl.addAll(url)
-        initRecyclerView()
-    }
-
-    private fun initRecyclerView() {
-        Log.d(TAG,"initRecyclerView: initialization")
-
-        var recyclerView: RecyclerView = requireView().findViewById(R.id.recycler_view)
-        var orientation = resources.configuration.orientation
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        //Adding the divider
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                R.drawable.recyclerview_divider
+            )
+        )
+        recyclerView.adapter = adapter
 
 //        TODO: In case someday we need to change the orientation of the RecyclerView. DO NOT DELETE IT
 //        recyclerView.layoutManager = LinearLayoutManager(requireContext(),
 //            if (orientation == Configuration.ORIENTATION_LANDSCAPE) LinearLayoutManager.HORIZONTAL
 //            else LinearLayoutManager.VERTICAL, false)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.addItemDecoration(DividerItemDecoration(requireContext(),R.drawable.recyclerview_divider))
 
-        var adapter = RecyclerViewAdapter(requireContext(), clientName,imgUrl,modelName,quantityNum)
-        recyclerView.adapter = adapter
-
+        return bind.root
     }
-
 }
