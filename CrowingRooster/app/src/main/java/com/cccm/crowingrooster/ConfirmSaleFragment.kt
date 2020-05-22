08952954303fce54bed77a583309cd93c15fbc49
Slackoth <00082318@uca.edu.sv>
2020-05-21 22:52:21 -0600

    [OnGoingSalesDetails UPDATED] - Confirm dialog added.

---
+++ CrowingRooster/app/src/main/java/com/cccm/crowingrooster/ConfirmSaleFragment.kt
@@ -0,0 +1,50 @@
+package com.cccm.crowingrooster
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsSpinner
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.cccm.crowingrooster.databinding.FragmentConfirmSaleBinding
+
+class ConfirmSaleFragment: DialogFragment() {
+
+    lateinit var bind: FragmentConfirmSaleBinding
+    lateinit var spinner: Spinner
+    lateinit var confirmTextView: TextView
+    lateinit var cancelTextView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        bind = DataBindingUtil.inflate(inflater,R.layout.fragment_confirm_sale, container,false)
+
+        bind.apply {
+            spinner = paymentSp
+            confirmTextView = confirmTv
+            cancelTextView = cancelTv
+        }
+
+        ArrayAdapter.createFromResource(requireContext(),R.array.payment_array,
+            android.R.layout.simple_spinner_item).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = it
+        }
+
+        cancelTextView.setOnClickListener{
+            dialog?.dismiss()
+        }
+        confirmTextView.setOnClickListener{
+
+        }
+
+        return bind.root
+    }
+}
--- CrowingRooster/app/src/main/java/com/cccm/crowingrooster/OngoingSalesDetailsFragment.kt
+++ CrowingRooster/app/src/main/java/com/cccm/crowingrooster/OngoingSalesDetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
 import androidx.databinding.DataBindingUtil
 import com.cccm.crowingrooster.databinding.FragmentOngoingSalesBinding
 import com.cccm.crowingrooster.databinding.FragmentOngoingSalesDetailsBinding
@@ -15,6 +17,7 @@ import com.cccm.crowingrooster.databinding.FragmentOngoingSalesDetailsBinding
 class OngoingSalesDetailsFragment : Fragment() {
 
     private lateinit var bind: FragmentOngoingSalesDetailsBinding
+    private lateinit var confirmButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +26,14 @@ class OngoingSalesDetailsFragment : Fragment() {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_ongoing_sales_details, container, false)
         bind = DataBindingUtil.inflate(inflater, R.layout.fragment_ongoing_sales_details, container, false)
+        bind.apply {
+            confirmButton = confirmBtt
+        }
+        confirmButton.setOnClickListener {
+            val dialog = ConfirmSaleFragment()
+            dialog.show(requireActivity().supportFragmentManager,"ConfirmDialog")
+        }
+
 
         return bind.root
     }
